9409149cc4ca28ec8b9d7f51605a78e793e798a5
Rob Hostetler <Macainian123@hotmail.com>
2016-01-25 15:31:09 -0500

    Bumped to 0.1.4

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-maced',
-    version='0.1.3',
+    version='0.1.4',
     # find_packages() takes a source directory and two lists of package name patterns to exclude and include.
     # If omitted, the source directory defaults to the same directory as the setup script.
     packages=find_packages(),
+    include_package_data=True,
     url='https://github.com/Macainian/Django-Maced',
     license='MIT License',
     author='Keith Hostetler',
