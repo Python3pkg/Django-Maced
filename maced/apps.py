ce9920ce96d65e34be73c2792fab0af8c50662af
Rob Hostetler <Macainian123@hotmail.com>
2016-01-29 12:06:25 -0500

    Changed the apps.py file.

--- maced/apps.py
+++ maced/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class DjangoMacedConfig(AppConfig):
-    name = 'django_maced'
+class MacedConfig(AppConfig):
+    name = 'maced'
