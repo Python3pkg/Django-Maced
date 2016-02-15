79aa84e165dd7b57278c293e7ae6343179f49781
Rob Hostetler <Macainian123@hotmail.com>
2016-02-15 17:21:08 -0500

    Resolved issue #36 and #27

--- maced/static/maced/js/maced.js
+++ maced/static/maced/js/maced.js
@@ -724,17 +724,11 @@ function get_authentication(item_name, main_action_type)
     // This suggests we have the wrong name for the select. Alternatively it was removed some how.
     if (item_select.length == 0)
     {
-        // There are cases where the message is irrelevant and so we will check the alert ignore list, and if it is in
-        // the list, we won't send an alert.
-        // http://stackoverflow.com/questions/237104/array-containsobj-in-javascript
-        if ($.inArray(item_name, maced_item_names_with_ignored_alerts) == -1)  // If not in ignore list
-        {
-            alert(
-                "The select with id \"" + item_name + "-select\" is not on the page. Perhaps the id is wrong or it was " +
-                "removed from the page dynamically or you didn't set is_used_only_for_maced_fields to True or it was " +
-                "just simply forgotten."
-            );
-        }
+        alert(
+            "The select with id \"" + item_name + "-select\" is not on the page. Perhaps the id is wrong or it was " +
+            "removed from the page dynamically or you didn't set is_used_only_for_maced_fields to True or it was " +
+            "just simply forgotten."
+        );
 
         return;
     }
