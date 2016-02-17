d1e426538d90e4dfdb4d1977dda0df7688a7dbda
Macainian <Macainian123@hotmail.com>
2016-02-17 04:51:27 -0500

    First massive movement for issue #44.

--- maced/utils/maced_creator.py
+++ maced/utils/maced_creator.py
@@ -490,47 +490,23 @@ def get_items_html_code_for_text(item_name, action_type, field_html_name, field_
     if action_type == MERGE:
         return get_merge_html_code_for_text(item_name, field_html_name, field_name)
 
-    html_code = '<b class="maced">' + field_html_name + ': </b>'
-    html_code += '<input type="text" class="maced form-control" id="' + action_type + '-' + item_name + '-' + field_name + '-input" '
+    context = {}
+    context["item_name"] = item_name
+    context["action_type"] = action_type
+    context["field_html_name"] = field_html_name
+    context["field_name"] = field_name
 
-    if action_type == DELETE or action_type == CLONE or action_type == INFO:
-        html_code += 'disabled '
-
-    html_code += '/>'
-
-    return html_code
+    return render(request=None, template_name="maced/inputs/text.html", context=context).content
 
 
 def get_merge_html_code_for_text(item_name, field_html_name, field_name):
-    # Create row name
-    html_code = '<tr class="maced">'
-    html_code += '<th class="maced">' + field_html_name + ': </th>'
+    context = {}
+    context["item_name"] = item_name
+    context["action_type"] = MERGE
+    context["field_html_name"] = field_html_name
+    context["field_name"] = field_name
 
-    # Create left panel
-    html_code += \
-        '<td class="maced">' \
-            '<input type="text" class="maced form-control" id="merge-' + item_name + '1-' + field_name + '-input" readonly />' \
-        '</td>'
-
-    html_code += '<th class="maced"> <button type="button" class="maced glyphicon glyphicon-arrow-right" style="color: Green; font-size: 20px"></button> </th>'
-
-    # Create middle panel
-    html_code += \
-        '<td class="maced" style="background-color: #F7D358;">' \
-            '<input type="text" class="maced form-control" id="merge-' + item_name + '-' + field_name + '-input" />' \
-        '</td>'
-
-    html_code += '<th class="maced"> <button type="button" class="maced glyphicon glyphicon-arrow-left" style="color: Green; font-size: 20px"></button> </th>'
-
-    # Create right panel for merge
-    html_code += \
-        '<td class="maced">' \
-            '<input type="text" class="maced form-control" id="merge-' + item_name + '2-' + field_name + '-input" readonly />' \
-        '</td>'
-
-    html_code += '</tr>'
-
-    return html_code
+    return render(request=None, template_name="maced/inputs/merge_text.html", context=context).content
 
 
 # COLOR
@@ -538,43 +514,23 @@ def get_items_html_code_for_color(item_name, action_type, field_html_name, field
     if action_type == MERGE:
         return get_merge_html_code_for_color(item_name, field_html_name, field_name)
 
-    html_code = '<b class="maced">' + field_html_name + ': </b>'
-    html_code += '<input type="color" class="maced form-control" id="' + action_type + '-' + item_name + '-' + field_name + '-input" value="#00FF00" '
+    context = {}
+    context["item_name"] = item_name
+    context["action_type"] = action_type
+    context["field_html_name"] = field_html_name
+    context["field_name"] = field_name
 
-    if action_type == DELETE or action_type == CLONE or action_type == INFO:
-        html_code += 'disabled '
-
-    html_code += '/>'
-
-    return html_code
+    return render(request=None, template_name="maced/inputs/color.html", context=context).content
 
 
 def get_merge_html_code_for_color(item_name, field_html_name, field_name):
-    # Create row name
-    html_code = '<tr class="maced">'
-    html_code += '<th class="maced">' + field_html_name + ': </th>'
+    context = {}
+    context["item_name"] = item_name
+    context["action_type"] = MERGE
+    context["field_html_name"] = field_html_name
+    context["field_name"] = field_name
 
-    # Create left panel
-    html_code += \
-        '<td class="maced">' \
-            '<input type="color" class="maced form-control" id="merge-' + item_name + '1-' + field_name + '-input" value="#00FF00" disabled />' \
-        '</td>'
-
-    # Create middle panel
-    html_code += \
-        '<td class="maced" style="background-color: #F7D358;">' \
-            '<input type="color" class="maced form-control" id="merge-' + item_name + '-' + field_name + '-input" value="#00FF00" />' \
-        '</td>'
-
-    # Create right panel for merge
-    html_code += \
-        '<td class="maced">' \
-            '<input type="color" class="maced form-control" id="merge-' + item_name + '2-' + field_name + '-input" value="#00FF00" disabled />' \
-        '</td>'
-
-    html_code += '</tr>'
-
-    return html_code
+    return render(request=None, template_name="maced/inputs/merge_color.html", context=context).content
 
 
 # SELECT
@@ -584,49 +540,25 @@ def get_items_html_code_for_select(item_name, action_type, field_html_name, fiel
     if action_type == MERGE:
         return get_merge_html_code_for_select(item_name, field_html_name, field_name, options_html_code)
 
-    html_code = '<b class="maced">' + field_html_name + ': </b>'
-    html_code += '<select class="maced form-control" id="' + action_type + '-' + item_name + '-' + field_name + '-input" '
+    context = {}
+    context["item_name"] = item_name
+    context["action_type"] = action_type
+    context["field_html_name"] = field_html_name
+    context["field_name"] = field_name
+    context["options_html_code"] = options_html_code
 
-    if action_type == DELETE or action_type == CLONE or action_type == INFO:
-        html_code += 'disabled '
-
-    html_code += '>' + options_html_code + '</select>'
-
-    return html_code
+    return render(request=None, template_name="maced/inputs/select.html", context=context).content
 
 
 def get_merge_html_code_for_select(item_name, field_html_name, field_name, options_html_code):
-    # Create row name
-    html_code = '<tr class="maced">'
-    html_code += '<th class="maced">' + field_html_name + ': </th>'
+    context = {}
+    context["item_name"] = item_name
+    context["action_type"] = MERGE
+    context["field_html_name"] = field_html_name
+    context["field_name"] = field_name
+    context["options_html_code"] = options_html_code
 
-    # Create left panel
-    html_code += \
-        '<td class="maced">' \
-            '<select class="maced form-control" id="merge-' + item_name + '1-' + field_name + '-input" readonly ' \
-                'onfocus="this.defaultIndex=this.selectedIndex;" onchange="this.selectedIndex=this.defaultIndex;">' + \
-                options_html_code + \
-            '</select>' \
-        '</td>'
-
-    # Create middle panel
-    html_code += \
-        '<td class="maced" style="background-color: #F7D358;">' \
-            '<select class="maced form-control" id="merge-' + item_name + '-' + field_name + '-input">' + options_html_code + '</select>' \
-        '</td>'
-
-    # Create right panel for merge
-    html_code += \
-        '<td class="maced">' \
-            '<select class="maced form-control" id="merge-' + item_name + '2-' + field_name + '-input" readonly ' \
-                'onfocus="this.defaultIndex=this.selectedIndex;" onchange="this.selectedIndex=this.defaultIndex;">' + \
-                options_html_code + \
-            '</select>' \
-        '</td>'
-
-    html_code += '</tr>'
-
-    return html_code
+    return render(request=None, template_name="maced/inputs/merge_select.html", context=context).content
 
 
 # OPTIONS FOR SELECT
